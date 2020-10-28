03647e27cf59f4756f751728b486cd11b1c6eda2
Tangyincheng <1069252020@qq.com>
2020-10-28 10:03:27 +0800

    添加首页css效果

--- blog-ui/components/Footer.js
+++ blog-ui/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
   <div className="footer-div">
     <div>Copyright © 2020 yctang 基于nextjs、eggjs、ant Design</div>
     {/* <div>基于nextjs、egjs、ant Design</div> */}
-    <div><a href="http://beian.miit.gov.cn/" target="_blank">蜀ICP备20009871号</a></div>
+    <div><a href="http://beian.miit.gov.cn/" target="_blank" style={{ color: '#007ca3' }}>蜀ICP备20009871号</a></div>
   </div>
 )
 
